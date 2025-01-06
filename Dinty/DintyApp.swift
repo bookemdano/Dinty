da7026856fe90b36813fd8d1d8aa2a92ba7cccbb
Dano <bookemdano@gmail.com>
2025-01-06 08:25:56 -0500

    Initial Commit

---
+++ Dinty/DintyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DintyApp.swift
+//  Dinty
+//
+//  Created by Daniel Francis on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DintyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
